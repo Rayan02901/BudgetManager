c1f9c59a5e63da97e1a0b4e3c0a6080971fcc3d2
“[Rayan <[rayan077@academiacollege.edu.np]>
2024-12-07 19:53:30 +0545

    initialization

---
+++ Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿using System;
+
+namespace BudgetManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.IncomeAmount = new System.Windows.Forms.TextBox();
+            this.ExpenseDescription = new System.Windows.Forms.TextBox();
+            this.ExpenseAmount = new System.Windows.Forms.TextBox();
+            this.AddIncome = new System.Windows.Forms.Button();
+            this.AddExpense = new System.Windows.Forms.Button();
+            this.IncomesListView = new System.Windows.Forms.ListView();
+            this.Total = new System.Windows.Forms.TextBox();
+            this.ConfirmExpenseCheckBox = new System.Windows.Forms.CheckBox();
+            this.UnconfirmedExpensesListView = new System.Windows.Forms.ListView();
+            this.ConfirmedExpensesListView = new System.Windows.Forms.ListView();
+            this.HelpLinkLabel = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Income Amount";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 118);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(131, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Expense Description";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(34, 159);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(108, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Expense Amount";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(34, 343);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Total";
+            // 
+            // IncomeAmount
+            // 
+            this.IncomeAmount.Location = new System.Drawing.Point(216, 30);
+            this.IncomeAmount.Name = "IncomeAmount";
+            this.IncomeAmount.Size = new System.Drawing.Size(123, 22);
+            this.IncomeAmount.TabIndex = 4;
+            // 
+            // ExpenseDescription
+            // 
+            this.ExpenseDescription.Location = new System.Drawing.Point(216, 111);
+            this.ExpenseDescription.Name = "ExpenseDescription";
+            this.ExpenseDescription.Size = new System.Drawing.Size(486, 22);
+            this.ExpenseDescription.TabIndex = 5;
+            // 
+            // ExpenseAmount
+            // 
+            this.ExpenseAmount.Location = new System.Drawing.Point(216, 152);
+            this.ExpenseAmount.Name = "ExpenseAmount";
+            this.ExpenseAmount.Size = new System.Drawing.Size(123, 22);
+            this.ExpenseAmount.TabIndex = 6;
+            // 
+            // AddIncome
+            // 
+            this.AddIncome.Location = new System.Drawing.Point(216, 71);
+            this.AddIncome.Name = "AddIncome";
+            this.AddIncome.Size = new System.Drawing.Size(132, 23);
+            this.AddIncome.TabIndex = 7;
+            this.AddIncome.Text = "Add Income";
+            this.AddIncome.UseVisualStyleBackColor = true;
+            // 
+            // AddExpense
+            // 
+            this.AddExpense.Location = new System.Drawing.Point(216, 233);
+            this.AddExpense.Name = "AddExpense";
+            this.AddExpense.Size = new System.Drawing.Size(132, 23);
+            this.AddExpense.TabIndex = 8;
+            this.AddExpense.Text = "Add Expense";
+            this.AddExpense.UseVisualStyleBackColor = true;
+            // 
+            // IncomesListView
+            // 
+            this.IncomesListView.HideSelection = false;
+            this.IncomesListView.Location = new System.Drawing.Point(354, 152);
+            this.IncomesListView.Name = "IncomesListView";
+            this.IncomesListView.Size = new System.Drawing.Size(180, 341);
+            this.IncomesListView.TabIndex = 9;
+            this.IncomesListView.UseCompatibleStateImageBehavior = false;
+            // 
+            // Total
+            // 
+            this.Total.Location = new System.Drawing.Point(189, 343);
+            this.Total.Name = "Total";
+            this.Total.Size = new System.Drawing.Size(100, 22);
+            this.Total.TabIndex = 10;
+            // 
+            // ConfirmExpenseCheckBox
+            // 
+            this.ConfirmExpenseCheckBox.AutoSize = true;
+            this.ConfirmExpenseCheckBox.Location = new System.Drawing.Point(37, 198);
+            this.ConfirmExpenseCheckBox.Name = "ConfirmExpenseCheckBox";
+            this.ConfirmExpenseCheckBox.Size = new System.Drawing.Size(137, 20);
+            this.ConfirmExpenseCheckBox.TabIndex = 11;
+            this.ConfirmExpenseCheckBox.Text = "Confirm Expense?";
+            this.ConfirmExpenseCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // UnconfirmedExpensesListView
+            // 
+            this.UnconfirmedExpensesListView.HideSelection = false;
+            this.UnconfirmedExpensesListView.Location = new System.Drawing.Point(548, 149);
+            this.UnconfirmedExpensesListView.Name = "UnconfirmedExpensesListView";
+            this.UnconfirmedExpensesListView.Size = new System.Drawing.Size(154, 342);
+            this.UnconfirmedExpensesListView.TabIndex = 12;
+            this.UnconfirmedExpensesListView.UseCompatibleStateImageBehavior = false;
+            // 
+            // ConfirmedExpensesListView
+            // 
+            this.ConfirmedExpensesListView.HideSelection = false;
+            this.ConfirmedExpensesListView.Location = new System.Drawing.Point(724, 149);
+            this.ConfirmedExpensesListView.Name = "ConfirmedExpensesListView";
+            this.ConfirmedExpensesListView.Size = new System.Drawing.Size(163, 341);
+            this.ConfirmedExpensesListView.TabIndex = 13;
+            this.ConfirmedExpensesListView.UseCompatibleStateImageBehavior = false;
+            // 
+            // HelpLinkLabel
+            // 
+            this.HelpLinkLabel.AutoSize = true;
+            this.HelpLinkLabel.Location = new System.Drawing.Point(615, 30);
+            this.HelpLinkLabel.Name = "HelpLinkLabel";
+            this.HelpLinkLabel.Size = new System.Drawing.Size(36, 16);
+            this.HelpLinkLabel.TabIndex = 14;
+            this.HelpLinkLabel.TabStop = true;
+            this.HelpLinkLabel.Text = "Help";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(942, 552);
+            this.Controls.Add(this.HelpLinkLabel);
+            this.Controls.Add(this.ConfirmedExpensesListView);
+            this.Controls.Add(this.UnconfirmedExpensesListView);
+            this.Controls.Add(this.ConfirmExpenseCheckBox);
+            this.Controls.Add(this.Total);
+            this.Controls.Add(this.IncomesListView);
+            this.Controls.Add(this.AddExpense);
+            this.Controls.Add(this.AddIncome);
+            this.Controls.Add(this.ExpenseAmount);
+            this.Controls.Add(this.ExpenseDescription);
+            this.Controls.Add(this.IncomeAmount);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Budget name:";
+            this.TextChanged += new System.EventHandler(this.Form1_TextChanged);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void Form1_TextChanged(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox IncomeAmount;
+        private System.Windows.Forms.TextBox ExpenseDescription;
+        private System.Windows.Forms.TextBox ExpenseAmount;
+        private System.Windows.Forms.Button AddIncome;
+        private System.Windows.Forms.Button AddExpense;
+        private System.Windows.Forms.ListView IncomesListView;
+        private System.Windows.Forms.TextBox Total;
+        private System.Windows.Forms.CheckBox ConfirmExpenseCheckBox;
+        private System.Windows.Forms.ListView UnconfirmedExpensesListView;
+        private System.Windows.Forms.ListView ConfirmedExpensesListView;
+        private System.Windows.Forms.LinkLabel HelpLinkLabel;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,302 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace BudgetManager
+{
+    public partial class Form1 : Form
+    {
+        // Lists to store income and expense records
+        private List<Income> incomes = new List<Income>();
+        private List<Expense> expenses = new List<Expense>();
+
+        // Tracks the current budget balance
+        private decimal budget = 0m;
+
+        // File path for persistent data storage
+        private string dataFilePath = "budget_data.txt";
+
+        public Form1()
+        {
+            // Standard Windows Forms initialization
+            InitializeComponent();
+
+            // Event handlers for button clicks and link interactions
+            AddIncome.Click += AddIncome_Click;
+            AddExpense.Click += AddExpense_Click;
+            HelpLinkLabel.LinkClicked += HelpLinkLabel_LinkClicked;
+
+            // Load existing data from file when application starts
+            LoadDataFromFile();
+        }
+
+        // Reads saved budget data from a text file
+        private void LoadDataFromFile()
+        {
+            if (File.Exists(dataFilePath))
+            {
+                // Read all lines from the data file
+                string[] lines = File.ReadAllLines(dataFilePath);
+                foreach (var line in lines)
+                {
+                    // Split each line into parts based on comma separator
+                    string[] parts = line.Split(',');
+
+                    // Reconstruct income entries
+                    if (parts[0] == "Income")
+                    {
+                        incomes.Add(new Income { Amount = decimal.Parse(parts[1]) });
+                    }
+                    // Reconstruct expense entries
+                    else if (parts[0] == "Expense")
+                    {
+                        expenses.Add(new Expense
+                        {
+                            Description = parts[1],
+                            Amount = decimal.Parse(parts[2]),
+                            IsConfirmed = bool.Parse(parts[3])
+                        });
+                    }
+                }
+                // Update budget and UI after loading data
+                UpdateBudget();
+                UpdateListViews();
+            }
+        }
+
+        // Writes current budget data to a text file for persistence
+        private void SaveDataToFile()
+        {
+            List<string> lines = new List<string>();
+
+            // Convert income entries to CSV format
+            foreach (var income in incomes)
+            {
+                lines.Add($"Income,{income.Amount}");
+            }
+
+            // Convert expense entries to CSV format
+            foreach (var expense in expenses)
+            {
+                lines.Add($"Expense,{expense.Description},{expense.Amount},{expense.IsConfirmed}");
+            }
+
+            // Write all lines to the data file
+            File.WriteAllLines(dataFilePath, lines.ToArray());
+        }
+
+        // Handles adding new income entries
+        private void AddIncome_Click(object sender, EventArgs e)
+        {
+            // Validate income amount input
+            if (decimal.TryParse(IncomeAmount.Text, out decimal amount) && amount > 0)
+            {
+                incomes.Add(new Income { Amount = amount });
+                IncomeAmount.Clear();
+
+                // Update budget, UI, and save data
+                UpdateBudget();
+                UpdateListViews();
+                SaveDataToFile();
+            }
+            else
+            {
+                // Show error for invalid income input
+                MessageBox.Show("Please enter a valid, positive income amount.", "Invalid Input", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        // Handles adding new expense entries
+        private void AddExpense_Click(object sender, EventArgs e)
+        {
+            // Validate expense description
+            string description = ExpenseDescription.Text.Trim();
+            if (string.IsNullOrEmpty(description))
+            {
+                MessageBox.Show("Please enter an expense description.", "Invalid Input", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // Validate expense amount input
+            if (decimal.TryParse(ExpenseAmount.Text, out decimal amount) && amount > 0)
+            {
+                // Check if expense is confirmed
+                bool isConfirmed = ConfirmExpenseCheckBox.Checked;
+
+                expenses.Add(new Expense
+                {
+                    Description = description,
+                    Amount = amount,
+                    IsConfirmed = isConfirmed
+                });
+
+                // Clear input fields
+                ExpenseDescription.Clear();
+                ExpenseAmount.Clear();
+                ConfirmExpenseCheckBox.Checked = false;
+
+                // Update budget, UI, and save data
+                UpdateBudget();
+                UpdateListViews();
+                SaveDataToFile();
+            }
+            else
+            {
+                // Show error for invalid expense input
+                MessageBox.Show("Please enter a valid, positive expense amount.", "Invalid Input", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        // Calculates current budget by summing incomes and subtracting confirmed expenses
+        private void UpdateBudget()
+        {
+            budget = 0m;
+
+            // Add all income amounts
+            foreach (var income in incomes)
+            {
+                budget += income.Amount;
+            }
+
+            // Subtract only confirmed expenses
+            foreach (var expense in expenses.Where(e => e.IsConfirmed))
+            {
+                budget -= expense.Amount;
+            }
+
+            // Display total budget in currency format
+            Total.Text = budget.ToString("C");
+        }
+
+        // Updates list views for incomes, unconfirmed and confirmed expenses
+        private void UpdateListViews()
+        {
+            // Populate Income ListView
+            IncomesListView.Items.Clear();
+            if (incomes.Count > 0)
+            {
+                IncomesListView.Items.Add(new ListViewItem(new[] { "Incomes", "Amount" }));
+                int incomeIndex = 1;
+                foreach (var income in incomes)
+                {
+                    IncomesListView.Items.Add(new ListViewItem(new[] { $"Income {incomeIndex}", income.Amount.ToString("C") }));
+                    incomeIndex++;
+                }
+            }
+
+            // Populate Unconfirmed Expenses ListView
+            UnconfirmedExpensesListView.Items.Clear();
+            var unconfirmedExpenses = expenses.Where(e => !e.IsConfirmed).ToList();
+            if (unconfirmedExpenses.Count > 0)
+            {
+                UnconfirmedExpensesListView.Items.Add(new ListViewItem(new[] { "Unconfirmed Expenses", "Amount" }));
+                foreach (var expense in unconfirmedExpenses)
+                {
+                    UnconfirmedExpensesListView.Items.Add(new ListViewItem(new[] { expense.Description, expense.Amount.ToString("C") }));
+                }
+            }
+
+            // Populate Confirmed Expenses ListView
+            ConfirmedExpensesListView.Items.Clear();
+            var confirmedExpenses = expenses.Where(e => e.IsConfirmed).ToList();
+            if (confirmedExpenses.Count > 0)
+            {
+                ConfirmedExpensesListView.Items.Add(new ListViewItem(new[] { "Confirmed Expenses", "Amount" }));
+                foreach (var expense in confirmedExpenses)
+                {
+                    ConfirmedExpensesListView.Items.Add(new ListViewItem(new[] { expense.Description, expense.Amount.ToString("C") }));
+                }
+            }
+        }
+
+        // Opens help dialog when help link is clicked
+        private void HelpLinkLabel_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            ShowHelpDialog();
+        }
+
+        // Displays help information in a modal dialog
+        private void ShowHelpDialog()
+        {
+            // Comprehensive help text explaining application usage
+            string helpText = @"Budget Manager Application Guide
+
+How to Use the Application:
+
+1. Adding Income
+- Enter the income amount in the 'Income Amount' field
+- Click 'Add Income' button
+- Income will be added to the Incomes list
+
+2. Adding Expenses
+- Enter expense description in 'Expense Description' field
+- Enter expense amount in 'Expense Amount' field
+- Use the 'Confirm Expense' checkbox to mark expense status
+   - Unchecked: Expense will appear in Unconfirmed Expenses list.
+     It means the Expense that You want to Carry out but are not
+     sure if it is within the budget. So this you will keep for 
+     Reminder and If you have enough Budget At the end of month then
+     You can confirm the Expense.
+   - Checked: Expense will appear in Confirmed Expenses list.
+     These are the expenses that have already been carried out or the 
+     compulsory expenses that you cannot Shy away from.
+
+3. Budget Calculation
+- Total budget is calculated as: 
+  Total = Sum of Incomes - Confirmed Expenses
+- The current budget is displayed in the Total field
+
+4. Data Persistence
+- All income and expense data is automatically saved
+- Data is stored in 'budget_data.txt' file
+- Data is loaded automatically when application starts
+
+Tips:
+- Always enter positive amounts
+- Use the 'Confirm Expense' checkbox to track expense status
+- Regularly review your income and expenses";
+
+            // Create a modal help dialog
+            using (Form helpForm = new Form())
+            {
+                helpForm.Text = "Budget Manager Help";
+                helpForm.Size = new Size(400, 500);
+                helpForm.FormBorderStyle = FormBorderStyle.FixedDialog;
+                helpForm.MaximizeBox = false;
+                helpForm.StartPosition = FormStartPosition.CenterScreen;
+
+                // Create a read-only multiline TextBox to display help text
+                TextBox helpTextBox = new TextBox
+                {
+                    Multiline = true,
+                    ReadOnly = true,
+                    Dock = DockStyle.Fill,
+                    Text = helpText,
+                    ScrollBars = ScrollBars.Vertical
+                };
+
+                helpForm.Controls.Add(helpTextBox);
+                helpForm.ShowDialog();
+            }
+        }
+    }
+
+    // Simple class to represent income entries
+    public class Income
+    {
+        public decimal Amount { get; set; }
+    }
+
+    // Class to represent expense entries with additional tracking
+    public class Expense
+    {
+        public string Description { get; set; }
+        public decimal Amount { get; set; }
+        public bool IsConfirmed { get; set; }
+    }
+}
